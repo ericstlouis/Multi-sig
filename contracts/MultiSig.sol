a91d93bede523524ed5a470da84d7e756b88a9c2
Eric St. Louis <ericstlouis2001@gmail.com>
2022-05-08 22:22:42 -0400

    Smart contract completed I think

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -1,41 +1,65 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.10;
-
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-
+/// @title Multi Sig
+/// @author Eric St. Louis
+/// @notice A Multisig using EIP-712 Standard
 contract MultiSig {
     using ECDSA for bytes32;
 
     //VARIABLES
+    ///@notice the number of signatures the needed to execute a transaction
     uint256 public numberOfSignatures;
+
+    ///@notice a list of signature that is eithier trusted or not
     mapping(address => bool) public isSigner;
+
+    //@notice how many times a transaction was perform
     uint public nonce;
 
+    ///@notice the components of the signautre
     struct Signatures {
         uint8 v;
         bytes32 r;
         bytes32 s;
     }
 
+    ///@notice EIP-712 domain separator 
     bytes32 public immutable domainSeparator;
 
+    ///@notice EIP-712 execute typehash
     bytes32 public constant EXECUTE_TYPEHASH = 
         keccak256('Execute(address to,uint256 value,bytes data,uint256 nonce)');
-
+    
+    ///@notice EIP-712 add signer typehash
      bytes32 public constant ADDSIGNER_TYPEHASH = 
         keccak256('AddSigners(address newSigner,bool shouldTrust,uint256 nonce');
 
     //EVENTS
 
+    ///@notice emitted if the execute function was sucessful
+    ///@param to the address the that recieving the eth
+    ///@param valueEth  the amount of eth
+    ///@param data the data of the transaction
+    ///@param previousLoop the signature address
     event Executed(address to, uint256 valueEth, bytes data, address previousLoop);
 
 
+    ///@notice emitted when a new signer is added
+    ///@param newSigner the address of the newSigner 
+    ///@param shouldTrust is the newSigner trrusted?
+    event UpdatedSigners(address newSigner, bool shouldTrust);
     //MODIFERS
 
 
     //FUNCTIONS
+
+    ///@notice deployed an instance on the multi-sig also set domainSeparator
+    ///@param name the name of the multisig
+    ///@param _signers the address os the signers
+    ///@param _numberOfSignatures the number of signatures that is required
     constructor(
         string memory name,
         address[] memory _signers,
@@ -62,6 +86,11 @@ contract MultiSig {
 
         }
 
+    ///@notice execute the transaction verifying the signatures
+    ///@param to the address the value is going to
+    ///@param data the data of the transaction
+    ///@param sigsVRS An array of the required signatures
+    ///@param value the amount Eth that is being sent to the to address
     function execute(
         address payable to, 
         bytes calldata data,
@@ -92,8 +121,13 @@ contract MultiSig {
     require(success, "execute failed to transact");
     }
 
+    ///@notice add a signer to the multiSig, also increment then amoount signatures required
+    ///@param newSigner the address of new signer
+    ///@param shouldTrust should the address be truseted
+    ///@param sigsVRS array of signatures
     function addSigners(
         address newSigner,
+        bool shouldTrust,
         Signatures[] memory sigsVRS
     ) public payable {
         require(isSigner[msg.sender]);
@@ -102,17 +136,27 @@ contract MultiSig {
         bytes32 hash = keccak256(
             abi.encodePacked(
                 '\x19\x01',
-                domainSepartor,
-                keccak256(abi.encode(arg);)
-            );
-        )
+                domainSeparator,
+                keccak256(abi.encode(ADDSIGNER_TYPEHASH, newSigner, shouldTrust, nonce++))
+            ));    
+            
+            address previousAddress;
 
+            for(uint256 i = 0; i < numberOfSignatures; i++ ) {
+                address currentLoopAddress = ecrecover(hash, sigsVRS[i].v, sigsVRS[i].r,sigsVRS[i].s);
 
+                require(!isSigner[currentLoopAddress] || previousAddress >= currentLoopAddress, "not valid Address");
 
-    }
+                previousAddress = currentLoopAddress;
+            }
 
-    function addSignatures() public {}
+            numberOfSignatures++;
+            isSigner[newSigner] = shouldTrust;
 
+        emit UpdatedSigners(newSigner, shouldTrust);
+        }
+    ///@notice get the balacnce of an account
+    ///@param ethAddress an account you want to get the addess from
      function getBalance(address ethAddress) public view returns (uint256) {
         return ethAddress.balance;
     }
--- test/MultiSigTest.js
+++ test/MultiSigTest.js
@@ -7,24 +7,25 @@ const hre = require('hardhat');
 
 
 describe("intialized MultiSig", function () {
-  let multiSigFactory, MultiSig, signer1, signer2, signer3, alice, numberOfSigs
-  
+  let multiSigFactory, MultiSig, signer1, signer2, signer3, alice, numberOfSigs;
+
   beforeEach(async () => {
     [signer1, signer2, signer3, alice] = await ethers.getSigners();
 
-    multiSigFactory = await ethers.getContractFactory("MultiSig");
+    multiSigFactory = await ethers.getContractFactory('MultiSig');
     MultiSig = await multiSigFactory.deploy(
       'test',
       [signer1.address, signer2.address, signer3.address],
       3
     );
     const Sigs = await MultiSig.numberOfSignatures();
-    numberOfSigs = Sigs.toNumber()
-
+    numberOfSigs = Sigs.toNumber();
   });
 
+  
 
-  it("sign typed data", async function() {
+
+  it('sign typed data', async function () {
     //EIP-712 data sig
     const domain = {
       name: 'test',
@@ -32,6 +33,93 @@ describe("intialized MultiSig", function () {
       chainId: 1337,
       verifyingContract: MultiSig.address,
     };
+
+    // The named list of all type definitions
+    const types = {
+      Person: [
+        { name: 'name', type: 'string' },
+        { name: 'wallet', type: 'address' },
+      ],
+      Mail: [
+        { name: 'from', type: 'Person' },
+        { name: 'to', type: 'Person' },
+        { name: 'contents', type: 'string' },
+      ],
+    };
+    // The data to sign
+    const value = {
+      from: {
+        name: 'Cow',
+        wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+      },
+      to: {
+        name: 'Bob',
+        wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+      },
+      contents: 'Hello, Bob!',
+    };
+    const signature1 = new Array(
+      await signer1._signTypedData(domain, types, value)
+    );
+    const signature2 = new Array(
+      await signer2._signTypedData(domain, types, value)
+    );
+    const signature3 = new Array(
+      await signer3._signTypedData(domain, types, value)
+    );
+
+    let sigArray = [signature1, signature2, signature3];
+
+    let newArray = new Array();
+
+    let ABI = ['function transfer(address to, uint amount)'];
+    let iface = new ethers.utils.Interface(ABI);
+
+    const data = iface.encodeFunctionData('transfer', [
+      alice.address,
+      parseEther('1.0'),
+    ]);
+
+    // console.log("data:", data)
+
+    for (let i = 0; i < sigArray.length; i++) {
+      for (let j = 0; j < sigArray[i].length; j++) {
+        const wholeSignature = sigArray[i][j];
+        const splitSig = wholeSignature.substring(2);
+        const r = '0x' + splitSig.substring(0, 64);
+        const s = '0x' + splitSig.substring(64, 128);
+        const v = parseInt(splitSig.substring(128, 130), 16);
+        // console.log("r:", r);
+        // console.log('s:', s);
+        // console.log('v:', v);
+        let SigArray = new Array(v, r, s);
+        newArray.push(SigArray);
+      }
+    }
+    // console.log("newArray:", newArray)
+
+    const firstBalance = await MultiSig.getBalance(alice.address);
+    firstFormatBalance = ethers.utils.formatEther(firstBalance);
+    console.log('pre-balance:', firstFormatBalance);
+
+    const executeTX = await MultiSig.execute(alice.address, data, newArray, 1, {
+      value: ethers.utils.parseEther('1.0'),
+    });
+
+    const secondBalance = await MultiSig.getBalance(alice.address);
+    secondFormatBalance = ethers.utils.formatEther(secondBalance);
+    console.log('post-balance:', secondFormatBalance);
+  });
+
+  it('Add signers', async function () {
+    //EIP-712 data sig
+    const domain = {
+      name: 'test',
+      version: '1',
+      chainId: 1337,
+      verifyingContract: MultiSig.address,
+    };
+
     // The named list of all type definitions
     const types = {
       Person: [
@@ -57,60 +145,67 @@ describe("intialized MultiSig", function () {
       contents: 'Hello, Bob!',
     };
 
+    
+    const signature1 = new Array(
+      await signer1._signTypedData(domain, types, value)
+    );
+    const signature2 = new Array(
+      await signer2._signTypedData(domain, types, value)
+    );
+    const signature3 = new Array(
+      await signer3._signTypedData(domain, types, value)
+    );
 
-    const signature1 = new Array(await signer1._signTypedData(domain, types, value));
-    const signature2 = new Array(await signer2._signTypedData(domain, types, value));
-    const signature3 = new Array(await signer3._signTypedData(domain, types, value));
-
-    let sigArray = [signature1, signature2, signature3]
+    
+    let sigArray = [signature1, signature2, signature3];
 
     let newArray = new Array();
 
-    let ABI = [
-    "function transfer(address to, uint amount)"
-];
+    let ABI = ['function transfer(address to, uint amount)'];
     let iface = new ethers.utils.Interface(ABI);
 
-   const data = iface.encodeFunctionData('transfer', [
-     alice.address,
-     parseEther('1.0'),
-   ]);
+    const data = iface.encodeFunctionData('transfer', [
+      alice.address,
+      parseEther('1.0'),
+    ]);
 
     // console.log("data:", data)
 
-
-
-    for (let i=0; i<sigArray.length; i++) {
-      for(let j = 0; j<sigArray[i].length; j++) {
+    for (let i = 0; i < sigArray.length; i++) {
+      for (let j = 0; j < sigArray[i].length; j++) {
         const wholeSignature = sigArray[i][j];
         const splitSig = wholeSignature.substring(2);
-        const r = "0x" + splitSig.substring(0, 64);
-        const s = "0x" + splitSig.substring(64, 128);
+        const r = '0x' + splitSig.substring(0, 64);
+        const s = '0x' + splitSig.substring(64, 128);
         const v = parseInt(splitSig.substring(128, 130), 16);
         // console.log("r:", r);
         // console.log('s:', s);
         // console.log('v:', v);
-        let SigArray = new Array(v,r,s)
+        let SigArray = new Array(v, r, s);
         newArray.push(SigArray);
       }
     }
-        // console.log("newArray:", newArray)
 
+    const firstAddressTrue = await MultiSig.isSigner(alice.address);
+    console.log(firstAddressTrue);
 
-    const firstBalance = await MultiSig.getBalance(alice.address);
-    firstFormatBalance = ethers.utils.formatEther(firstBalance);
-    console.log('pre-balance:', firstFormatBalance);
+    const firstSignature = await MultiSig.numberOfSignatures();
+    console.log(firstSignature)
+    const executeTx = await MultiSig.addSigners(alice.address, true, newArray, {value: ethers.utils.parseEther("1.0")})
+    const secondSignature = await MultiSig.numberOfSignatures();
+    console.log(secondSignature)
 
-    const executeTX = await MultiSig.execute(alice.address, data, newArray, 1, {
-      value: ethers.utils.parseEther('1.0')
-    });
-
-    const secondBalance = await MultiSig.getBalance(alice.address);
-    secondFormatBalance = ethers.utils.formatEther(secondBalance);
-    console.log("post-balance:", secondFormatBalance)
-
-  })
+    const secondAddressTrue = await MultiSig.isSigner(alice.address);
+    console.log(secondAddressTrue)
+  });
 });
+
+
+
+
+
+
+
 
 
 
